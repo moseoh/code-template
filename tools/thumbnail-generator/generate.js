5039e8625a203a86062fee06cf76a09544e74ef0
moseoh <azqazq195@gmail.com>
2026-01-22 08:43:47 +0900

    refactor(thumbnail-generator): 설정 파일을 config 폴더로 이동하고 assets/output 경로 분리
    - config.json, config-bookreport.json을 config/logo.json, config/book-cover.json으로 이동
    - assets/logo, assets/book-cover로 입력 경로 분리
    - output/logo, output/book-cover로 출력 경로 분리
    - CLI config 옵션 제거하고 로고/책표지 설정을 순차 실행하도록 변경
    - commander 의존성 제거

--- tools/thumbnail-generator/generate.js
+++ tools/thumbnail-generator/generate.js
@@ -1,17 +1,13 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-import { AutoThumbnailGenerator } from './auto-generator.js';
+import { AutoThumbnailGenerator } from "./auto-generator.js";
 
-const program = new Command();
+const configPaths = ["./config/logo.json", "./config/book-cover.json"];
 
-program
-  .name('thumbnail-generator')
-  .description('로고와 배경색을 사용한 썸네일 이미지 생성기')
-  .option('-c, --config <path>', 'config 파일 경로 지정', './config.json')
-  .parse();
-
-const options = program.opts();
-
-const generator = new AutoThumbnailGenerator(options.config);
-await generator.generateAll();
+for (const configPath of configPaths) {
+  const generator = new AutoThumbnailGenerator(configPath);
+  await generator.generateAll();
+  console.log();
+  console.log("============================");
+  console.log();
+}
